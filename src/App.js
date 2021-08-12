469b25c5cb08fcc0f073dbfb2e97b5d04f5aa2ef
Jesus Navarrete <jesusnavarrete@Jesuss-MacBook-Pro.local>
2021-08-11 19:49:30 -0700

    finished

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
+import Title from "./Components/title";
+import Explanation from "./Components/explanation";
+import Copyright from "./Components/copyright";
+import Date from "./Components/date";
 
 function App() {
+  const [nasaData, setNasaData] = useState({}); //slice of data
+
+  useEffect(() => {
+    axios
+      .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`) //network request
+      .then((res) => {
+        console.log(res);
+        setNasaData(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const img = document.createElement("img");
+  img.src = nasaData.url;
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div>
+        <Title title={nasaData.title} />
+      </div>
+      <div>
+        <Date date={nasaData.date} />
+      </div>
+      <div>
+        <img src={nasaData.url} />
+      </div>
+      <div>
+        <Copyright copyright={nasaData.copyright} />
+      </div>
+      <div>
+        <Explanation explanation={nasaData.explanation} />
+      </div>
     </div>
   );
 }
---
+++ src/Components/date.js
@@ -0,0 +1,5 @@
+import React, { useState, useEffect } from "react";
+
+export default function Date(props) {
+  return <h3>{props.date}</h3>;
+}
